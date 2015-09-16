12bdc98ca4554ce76a2d468380c167930181f8cf
Ryan Scott Lewis <ryan@rynet.us>
2015-09-16 02:47:20 -0400

    All drawing methods within component

--- lib/cura/component/base.rb
+++ lib/cura/component/base.rb
@@ -116,11 +116,37 @@ module Cura
       
       protected
       
+      # Draw a point.
+      def draw_point(x, y, color=Cura::Color.black)
+        x = absolute_x + @offsets.left + x
+        y = absolute_y + @offsets.top + y
+        
+        pencil.draw_point(x, y, width, height, color)
+      end
+      
+      # Draw a rectangle.
+      # TODO: filled argument
+      def draw_rectangle(x, y, width, height, color=Cura::Color.black)
+        x = absolute_x + @offsets.left + x
+        y = absolute_y + @offsets.top + y
+        
+        pencil.draw_rectangle(x, y, width, height, color)
+      end
+      
+      # Draw a single character.
       def draw_character(x, y, character, foreground=Cura::Color.black, background=Cura::Color.white, bold=false, underline=false)
         x = absolute_x + @offsets.left + x
         y = absolute_y + @offsets.top + y
         
         pencil.draw_character(x, y, character, foreground, background, bold, underline)
+      end
+      
+      # Draw text.
+      def draw_text(x, y, text, foreground=Cura::Color.black, background=Cura::Color.white, bold=false, underline=false)
+        x = absolute_x + @offsets.left + x
+        y = absolute_y + @offsets.top + y
+        
+        pencil.draw_text(x, y, text, foreground, background, bold, underline)
       end
       
       # Draw the background of this component.
