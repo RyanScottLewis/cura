e308affec0b723f190ce0225202c0f9a9002c6d9
Ryan Scott Lewis <ryan@rynet.us>
2015-09-15 20:27:53 -0400

    Small MRuby gem fix

--- src/gem_init.c
+++ src/gem_init.c
@@ -1,7 +1,7 @@
 #include "mruby.h"
 
-void mrb_mruby_cura_gem_init( mrb_state *mrb ) {
+void mrb_cura_gem_init( mrb_state *mrb ) {
 }
 
-void mrb_mruby_cura_gem_final( mrb_state *mrb ) {
+void mrb_cura_gem_final( mrb_state *mrb ) {
 }
