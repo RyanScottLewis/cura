d8ea51acd943d485c80850cd02780ef620c131b8
Ryan Scott Lewis <ryan@rynet.us>
2016-07-11 04:18:54 -0400

    TreeController::Ancestry updates, use Helpers::Validations

--- lib/cura/tree_controller/ancestry.rb
+++ lib/cura/tree_controller/ancestry.rb
@@ -1,22 +1,48 @@
+if Kernel.respond_to?(:require)
+  require "cura/attributes/has_ancestry"
+  require "cura/attributes/has_children"
+
+  require "cura/helpers/validations"
+end
+
 module Cura
   class TreeController
     module Ancestry
+      include Helpers::Validations
+
+      # Creates a relation, or "edge", within a component view tree.
       def create_relation(parent, child)
-        child.parent = parent unless child.parent == parent
+        validate_type(parent, Attributes::HasApplication)
+        validate_type(parent, Attributes::HasChildren)
+
+        validate_type(child,  Attributes::HasAncestry)
+
+        return [parent, child] if child.parent == parent && parent.has_child?(child)
+
+        child.set_parent(parent) unless child.parent == parent
         parent.children << child unless parent.has_child?(child)
+        parent.set_children(parent.children + [child]) unless parent.has_child?(child)
 
         child.root = parent.root.nil? ? parent : parent.root
+
         child.ancestors = find_ancestors(child)
+
+        child.application = parent.application
 
         [parent, child]
       end
 
       protected
 
-      def find_ancestors(node)
+      def find_ancestors(node, memo=[])
+        validate_type(node, Attributes::HasAncestry)
 
+        return memo unless node.parent?
+
+        memo << node.parent
+
+        find_ancestors(node.parent, memo=[])
       end
-
     end
   end
 end
