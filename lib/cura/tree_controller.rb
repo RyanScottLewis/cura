9cdb896ba4a6b9e4ef8abdd8e19036e01685758f
Ryan Scott Lewis <ryan@rynet.us>
2016-07-11 04:15:21 -0400

    Check for require call in TreeController

--- lib/cura/tree_controller.rb
+++ lib/cura/tree_controller.rb
@@ -1,10 +1,14 @@
-require "cura/tree_controller/ancestry"
-require "cura/tree_controller/attribute_inheritence"
+if Kernel.respond_to?(:require)
+  require "cura/tree_controller/ancestry"
+  require "cura/tree_controller/attribute_inheritence"
+end
 
 module Cura
   # Controls a component view tree.
   #
   # Effectively decouples nodes, preventing them from controlling the internal state of other nodes.
+  # The trade off is more method calls during creation and modification of components, but faster
+  # drawing.
   class TreeController
     include Ancestry
     include AttributeInheritence
