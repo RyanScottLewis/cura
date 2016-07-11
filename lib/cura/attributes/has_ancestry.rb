b03ea1b8fe067991d36b8c3ca425aba3e548e9b1
Ryan Scott Lewis <ryan@rynet.us>
2016-07-11 04:15:41 -0400

    Attributes::HasAncestry updates

--- lib/cura/attributes/has_ancestry.rb
+++ lib/cura/attributes/has_ancestry.rb
@@ -1,12 +1,17 @@
 if Kernel.respond_to?(:require)
-  require "cura/error/invalid_component"
-  require "cura/component/base"
+  require "cura/helpers/conversions"
+
+  require "cura/attributes/has_application"
+
+  require "cura/tree_controller"
 end
 
 module Cura
   module Attributes
     # Allows an object to have a `parent` and `ancestors`.
     module HasAncestry
+      include HasApplication
+      include Helpers::Conversions
 
       def initialize(attributes={})
         @ancestors = [].freeze
@@ -17,32 +22,41 @@ module Cura
       # Get the parent of this object.
       #
       # @return [Component::Base]
-      attr_accessor :parent
+      attr_reader :parent
 
-      # Set the parent of this object.
-      # This modifies the state of the view tree.
-      #
-      # @param [Component::Base] value
-      # @return [Component::Base]
-      def parent=(value)
-        raise Error::InvalidComponent unless value.is_a?(Component::Base)
-
-        @parent = value
-        @application.tree_controller.create_relation(@parent, self)
-
-        @parent
-      end
-
-      # Determine if this object has a parent.
+      # Get whether this object has a parent.
       #
       # @return [Boolean]
       def parent?
         !@parent.nil?
       end
 
+      # Set the parent of this object.
+      #
+      # @param [Component::Base] value
+      # @return [Component::Base]
+      def parent=(value)
+        @application.tree_controller.create_relation(value, self)
+
+        @parent
+      end
+
+      # Set the parent of this object.
+      #
+      # Usage of this method does not update any other aspects of the relationship between the parent and this
+      # component and should only be used internally.
+      #
+      # @param [Component::Base] value
+      # @return [Component::Base]
+      def set_parent(value)
+        validate_type(value, Component::Base)
+
+        @parent = value
+      end
+
       # Get the ancestors of this object.
       #
-      # @return [<Object>]
+      # @return [<Component::Base>]
       attr_reader :ancestors
 
       # Set the ancestors of this object.
@@ -51,7 +65,7 @@ module Cura
       # @param [<Component::Base>] value
       # @return [<Component::Base>]
       def ancestors=(value)
-        @ancestors = value.to_a.keep_if { |object| object.is_a?(Component::Base) }.freeze
+        @ancestors = convert_array_instances(value, Component::Base).freeze
       end
 
     end
